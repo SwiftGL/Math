59982ae9e97fffc544172c8fa6934dc9120cffb4
Christian Treffs <ctreffs@gmail.com>
2019-08-23 14:54:33 +0200

    Update to Swift 5

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,33 +1,8 @@
-// Copyright (c) 2015-2016 David Turnbull
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and/or associated documentation files (the
-// "Materials"), to deal in the Materials without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Materials, and to
-// permit persons to whom the Materials are furnished to do so, subject to
-// the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Materials.
-//
-// THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
-
 import XCTest
-@testable import SGLMathTests
 
-XCTMain([
-    testCase(SwizzleTests.allTests),
-    testCase(Vector2Tests.allTests),
-    testCase(Vector4Tests.allTests),
-    testCase(FunctionsTests.allTests),
-    testCase(Matrix2x2Tests.allTests),
-    testCase(Matrix3x3Tests.allTests),
-    testCase(Matrix4x4Tests.allTests)
-])
+import SGLMathTests
+
+var tests = [XCTestCaseEntry]()
+tests += SGLMathTests.__allTests()
+
+XCTMain(tests)
